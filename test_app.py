4e8c395164d364f3e6c903515d41740a72973736
Eduard Usatchev <eduard.usatchev@gmail.com>
2024-07-25 17:50:12 +0300

    jenkins if statement

--- test_app.py
+++ test_app.py
@@ -15,7 +15,7 @@ class FlaskAppTestCase(unittest.TestCase):
         # Assert the response status code is 200 (OK)
         self.assertEqual(response.status_code, 200)
         # Assert the response data is 'Hello, World!'
-        self.assertEqual(response.data.decode(), f'Hello, World! {name}')
+        self.assertEqual(response.data.decode(), f'Hello, World! {name}dfhlkgmsgflhk')
 
 
 if __name__ == '__main__':
